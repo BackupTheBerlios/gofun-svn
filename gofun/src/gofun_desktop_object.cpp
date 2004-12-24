bbcc37fbf4590b0788b2577c65b4c93f71b799b0
tobgle <tobgle@5fd62da2-93e7-0310-a849-e8a8a5672372>
2004-12-24 22:53:28 +0000

    This files were missing too in the repository ...

---
+++ gofun/src/gofun_desktop_object.cpp
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Tobias Glaesser                                 *
+ *   tobi.web@gmx.de                                                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <qfile.h>
+ 
+#include "gofun_desktop_object.h"
+#include "gofun_data.h"
+
+void GofunDesktopObject::save()
+{
+	GofunDesktopEntryData* pdata = reinterpret_cast<GofunDesktopEntryData*>(data());
+	QFile file( pdata->File );
+	if ( file.open( IO_WriteOnly ) )
+	{
+		QTextStream stream( &file );
+		stream << "[Desktop Entry]\n";
+		stream << "Version=0.9.4\n";
+		stream << "Encoding=UTF-8\n";
+		stream << "Type=Directory\n";
+		stream << "Name=" << pdata->Name << "\n";
+		stream << "Comment=" << pdata->Comment << "\n";
+		stream << "Icon=" << pdata->Icon << "\n";
+		file.close();
+	}
+}
+
