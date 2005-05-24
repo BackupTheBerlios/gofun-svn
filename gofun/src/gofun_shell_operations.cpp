4fb414dee743f85527576627b9ff3cd542f3cf7e
tobgle <tobgle@5fd62da2-93e7-0310-a849-e8a8a5672372>
2005-05-24 19:12:47 +0000

    added #include <stdio.h> to be more compatible to different gcc versions

--- gofun/src/gofun_shell_operations.cpp
+++ gofun/src/gofun_shell_operations.cpp
@@ -18,6 +18,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
+#include <stdio.h>
+
 #include "gofun_shell_operations.h"
 
 QString GofunShellOperations::shellCall(const QString& call)
